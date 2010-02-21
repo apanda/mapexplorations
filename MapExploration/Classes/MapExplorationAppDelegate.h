9e4cc04f02d46f2d89cec7a3ce3b8b318ef3ff14
Aurojit Panda <aurojit@gmail.com>
2010-02-20 23:35:02 -0800

    Create new xcode project

---
+++ MapExploration/Classes/MapExplorationAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  MapExplorationAppDelegate.h
+//  MapExploration
+//
+//  Created by Aurojit Panda on 2/20/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface MapExplorationAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
